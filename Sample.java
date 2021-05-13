b671fbc3fce519db0850eea85ed10536a7369c0f
anbumithesh <76044035+anbumithesh@users.noreply.github.com>
2021-05-13 13:06:24 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class HelloGuys{
   public static void main(String[] args) {
-    System.out.println("hey Anbu");
+    System.out.println("hey Anbunathan");
     Sytem.out.println("Hey Mithesh");
     Sytem.out.println("Hey Rathika");
   }
